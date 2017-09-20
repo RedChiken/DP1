7dc6a610e5f249f96283393ef36b0c1b968c1e56
RedChiken <gombabo1217@gmail.com>
2017-09-20 17:11:35 +0900

    Add HW code

---
+++ src/oopreview/practice4/Worker.java
@@ -0,0 +1,27 @@
+package oopreview.practice4;
+
+public abstract class Worker {
+   public abstract void doWork();   
+    // fill necessary fields
+    
+    public double getSalary() { 
+        // fill necessary codes
+   }
+    
+    public void setSalary(double salary) {
+        // fill necessary codes
+    }
+
+    public void increaseSalary(double rate) {
+        // fill necessary codes
+    }
+    
+    public void setEmployeeType(EmployeeType emptype) {
+        // fill necessary codes
+    }
+    
+    public EmployeeType getEmpoyeeType() {
+        // fill necessary codes
+    }    
+   
+}
